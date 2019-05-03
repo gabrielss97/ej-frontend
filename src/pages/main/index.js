662fe4578eae3967e1103d34c2a084a4858c33d9
gabrielss97 <gabrielss97@users.noreply.github.com>
2019-05-03 15:13:59 -0300

    message err

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,36 +1,40 @@
-import React, { Component } from "react";
-import api from '../../services/api';
+import React, { Component } from "../../../node_modules/react";
+import api from "../../services/api";
 import "./styles.css";
 
 export default class Main extends Component {
   state = {
     newUser: {
-      username: '',
-      password: '',
-      password2: '',
-      email: '',
-    }
+      username: "",
+      password: "",
+      password2: "",
+      email: ""
+    },
+
+    responseMessage: ""
   };
 
   handleSubmit = async e => {
     e.preventDefault();
-    console.log(this.state.newUser);
     const response = await api.post("users", {
-      username: this.state.username,
-      email: this.state.email,
-      password: this.state.password,
-      password2: this.state.password2
+      username: this.state.newUser.username,
+      email: this.state.newUser.email,
+      password: this.state.newUser.password,
+      password2: this.state.newUser.password2
     });
 
-    console.log(response.data);
+    console.log("response", response.data);
+    if (!response.data.success) {
+      this.setState({ responseMessage: response.data.message });
+    } else {
+      this.setState({ responseMessage: "" });
+    }
   };
 
-  handleInputChange = (e) => {
-    this.setState({username: e.target.value});
-    this.setState({email: e.target.value});
-    this.setState({password: e.target.value});
-    this.setState({password2: e.target.value});
-
+  handleInputChange = e => {
+    const newUser = this.state.newUser;
+    newUser[e.target.id] = e.target.value;
+    this.setState({ newUser });
   };
 
   render() {
@@ -41,29 +45,39 @@ export default class Main extends Component {
             <i className="fas fa-pencil-ruler" />
             <h1>Troca Justa</h1>
 
-            <input 
-            placeholder="Usuário" 
-            type="text"
-            defaultValue={this.state.newUser.username}
-            onChange={this.handleInputChange} />
+            <input
+              placeholder="Usuário"
+              type="text"
+              id="username"
+              defaultValue={this.state.newUser.username}
+              onChange={this.handleInputChange}
+            />
 
-            <input 
-            placeholder="E-mail" 
-            type="email"
-            defaultValue={this.state.newUser.email}
-            onChange={this.handleInputChange} />
+            <input
+              placeholder="E-mail"
+              type="email"
+              id="email"
+              defaultValue={this.state.newUser.email}
+              onChange={this.handleInputChange}
+            />
 
-            <input placeholder="Senha" 
-            type="password" 
-            id="password"
-            defaultValue={this.state.newUser.password}
-            onChange={this.handleInputChange} />
+            <input
+              placeholder="Senha"
+              type="password"
+              id="password"
+              defaultValue={this.state.newUser.password}
+              onChange={this.handleInputChange}
+            />
 
-            <input placeholder="Repita senha"
-             type="password"
-             id="password2"
-             defaultValue={this.state.newUser.password2}
-             onChange={this.handleInputChange} />
+            <input
+              placeholder="Repita senha"
+              type="password"
+              id="password2"
+              defaultValue={this.state.newUser.password2}
+              onChange={this.handleInputChange}
+            />
+
+            <p className="response-message">{this.state.responseMessage}</p>
 
             <button type="submit">Cadastrar</button>
             <p>Faça Login</p>
